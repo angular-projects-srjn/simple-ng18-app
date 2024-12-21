23dcc4f65676f10866db77af246a69f2358dc9c7
Sarjan <sarjan.ahmad@ebsconsulting.biz>
2024-12-21 12:04:19 +0530

    Added server file

---
+++ angular_18_tutorial-main/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+const port = process.env.PORT || 4200;
+
+// importing the dotenv module to use environment variables:
+require("dotenv").config();
+
+app.use(express.static(resolve(__dirname, "./src")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const path = resolve("./src" + "/index.html");
+  res.sendFile(path);
+});
+
+
+// Server listening:
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+  // console.log(`You may access you app at: ${domainURL}`);
+});
